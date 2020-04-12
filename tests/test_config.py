9fdf99c05d23f27fd79b6da8fe58d083c1fe5de3
Jason Paris <paris3200@gmail.com>
2020-04-12 08:36:36 -0400

    Add Config test

---
+++ tests/test_config.py
@@ -0,0 +1,10 @@
+"""
+Test the Config Class
+"""
+from pacioli.config import Config
+
+
+def test_profile_set():
+    config = Config("tests/sample_config.yml")
+    assert config.effective == "--effective"
+    assert config.journal_file == "tests/ledger.ldg"
