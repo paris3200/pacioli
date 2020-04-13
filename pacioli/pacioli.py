e5bf6b34927fa7ea3a6b248a84aafdf819499e2b
Jason Paris <paris3200@gmail.com>
2020-04-13 19:37:36 -0400

    Document Pacioli class

--- pacioli/pacioli.py
+++ pacioli/pacioli.py
@@ -1,14 +1,23 @@
 import os
 import subprocess
 import re
-
 import jinja2
-from jinja2 import Template
+
 
 from pacioli.config import Config
 
 
 class Pacioli:
+    """
+    Pacioli converts a ledger journal file data into a finacial reports
+    formatted with LaTex.  The LaTeX reports can then be piped into a LaTeX to
+    pdf convertor inorder to generate beautiful, accurate reports.
+
+    Paramaters
+    ----------
+    config_file: str
+        Path to the config file.
+    """
 
     def __init__(self, config_file=None):
 
@@ -25,49 +34,51 @@ class Pacioli:
             autoescape=False,
             loader=jinja2.FileSystemLoader(os.path.abspath(".")),
         )
-        
+
         self.config = Config(config_file)
         self.date = "2020/3/31"
 
     def get_balance(self, account):
-        output = subprocess.run(["ledger", "-f",
-            self.config.journal_file, "bal", account, "-e", self.date,
-            self.config.effective], stdout=subprocess.PIPE)
-        output = output.stdout.decode('utf-8')
-        output = output.replace(',', '')
+        """
+        Get's the account balance from Ledger of account and rounds it to a
+        whole number.
+
+        Parameters
+        ----------
+        account: str
+            The account name in the ledger file.
+        """
+        output = subprocess.run(
+            [
+                "ledger",
+                "-f",
+                self.config.journal_file,
+                "bal",
+                account,
+                "-e",
+                self.date,
+                self.config.effective,
+            ],
+            stdout=subprocess.PIPE,
+        )
+        output = output.stdout.decode("utf-8")
+        output = output.replace(",", "")
         return round(float(re.search("\d+(?:.(\d+))?", output).group(0)))
 
     def compile_template(self, **kwargs):
-        template = self.latex_jina_env.get_template("balance_sheet.tex")
-        print(template.render(checking=checking, \
-                savings=savings, \
-                current_assets=current_assets, \
-                escrow=escrow, \
-                realestate=realestate, \
-                investments=investments, \
-                receivable=receivable, \
-                longterm_assets=longterm_assets, \
-                total_assets=total_assets))
+        template = self.latex_jina_env.get_template("pacioli/balance_sheet.tex")
 
-
-if __name__ == "__main__":
-
-    pacioli = Pacioli()
-
-    checking = pacioli.get_balance("Assets:Current:SECU:Checking")
-    savings = pacioli.get_balance("Assets:Current:SECU:Savings")
-    current_assets = round(checking) + round(savings)
-    current_assets = 10
-
-    escrow = pacioli.get_balance("Assets:Noncurrent:SECU:Escrow")
-    realestate = pacioli.get_balance("Assets:Noncurrent:House")
-    investments = pacioli.get_balance("Assets:Noncurrent:Retirement")
-    receivable = pacioli.get_balance("Assets:Noncurrent:Parisleatherworks")
-
-
-    longterm_assets = round(escrow) + round(realestate)+ round(investments) + round(receivable)
-    total_assets = current_assets + longterm_assets
-
-    pacioli.compile_template()
-
-
+        return template.render(kwargs)
+        # print(
+        #    template.render(
+        #        checking=checking,
+        #        savings=savings,
+        #        current_assets=current_assets,
+        #        escrow=escrow,
+        #        realestate=realestate,
+        #        investments=investments,
+        #        receivable=receivable,
+        #        longterm_assets=longterm_assets,
+        #        total_assets=total_assets,
+        #    )
+        # )
