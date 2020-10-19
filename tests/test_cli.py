4d5de5be7a165f3e2b1f780e7dcedf3843579540
Jason Paris <paris3200@gmail.com>
2020-10-19 16:56:56 -0400

    Add test for writing income statement to file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -35,7 +35,7 @@ def test_balance_sheet_outputs_to_standard_output():
     assert "{Total Secured Liabilities} & & 184654\\" in report
 
 
-def test_income_sheet_outputs_to_standard_output():
+def test_income_statement_outputs_to_standard_output():
     """
     Balance sheet returns a formatted report to standard output.
     """
@@ -54,7 +54,6 @@ def test_income_sheet_outputs_to_standard_output():
         stdout=subprocess.PIPE,
     )
     report = output.stdout.decode()
-    print(report)
     assert "Acme LLC" in report
     assert "Salary & 4913 \\" in report
     assert "{Total Income}} & & 4953" in report
@@ -91,3 +90,35 @@ def test_balance_sheet_outputs_to_file(tmp_path):
     assert "{Total Longterm Assets} & & 325576" in report
     assert "{Total Current Assets}} & & 14168\\" in report
     assert "{Total Secured Liabilities} & & 184654\\" in report
+
+
+def test_income_statment_outputs_to_file(tmp_path):
+    """
+    Balance sheet writes to file.
+    """
+
+    # Create a temp directory
+    d = tmp_path / "report"
+    d.mkdir()
+
+    output_file = d / "report.tex"
+
+    subprocess.run(
+        [
+            "pacioli",
+            "-c",
+            "tests/resources/sample_config.yml",
+            "income-statement",
+            "--begin-date",
+            "2020/2/1",
+            "--end-date",
+            "2020/3/31",
+            output_file,
+        ],
+        stdout=subprocess.PIPE,
+    )
+    report = output_file.read_text()
+    assert "Acme LLC" in report
+    assert "Salary & 4913 \\" in report
+    assert "{Total Income}} & & 4953" in report
+    assert "{Total Expenses}} & & 4162 \\" in report
