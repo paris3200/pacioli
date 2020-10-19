049be7ee2316e1361c0b433fd0a089dd0c3f903c
Jason Paris <paris3200@gmail.com>
2020-10-19 15:55:14 -0400

    Add Test for Balance Statement

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -35,6 +35,32 @@ def test_balance_sheet_outputs_to_standard_output():
     assert "{Total Secured Liabilities} & & 184654\\" in report
 
 
+def test_income_sheet_outputs_to_standard_output():
+    """
+    Balance sheet returns a formatted report to standard output.
+    """
+    output = subprocess.run(
+        [
+            "pacioli",
+            "-c",
+            "tests/resources/sample_config.yml",
+            "income-statement",
+            "--begin-date",
+            "2020/2/1",
+            "--end-date",
+            "2020/3/31",
+            "-",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    report = output.stdout.decode()
+    print(report)
+    assert "Acme LLC" in report
+    assert "Salary & 4913 \\" in report
+    assert "{Total Income}} & & 4953" in report
+    assert "{Total Expenses}} & & 4162 \\" in report
+
+
 def test_balance_sheet_outputs_to_file(tmp_path):
     """
     Balance sheet writes to file.
